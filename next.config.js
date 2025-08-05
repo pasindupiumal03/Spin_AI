f809d28b6e700fb2a603633af9a433e8ed1cb155
Pasindu <163386074+pasindupiumal03@users.noreply.github.com>
2025-08-06 01:33:03 +0530

    updates

--- next.config.js
+++ next.config.js
@@ -5,6 +5,23 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  transpilePackages: ['@codesandbox/sandpack-react'],
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
+    ignoreBuildErrors: true,
+  },
+  webpack: (config, { isServer }) => {
+    // Add a rule to handle .d.ts files
+    config.module.rules.push({
+      test: /\.d\.ts$/,
+      use: 'raw-loader',
+    });
+    
+    return config;
+  },
 };
 
 module.exports = nextConfig;
